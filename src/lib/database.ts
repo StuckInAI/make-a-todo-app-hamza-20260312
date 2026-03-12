d37918e2653015c56a1117a4817211198c521c03
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-12 15:05:42 +0500

    Add src/lib/database.ts

---
+++ src/lib/database.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Todo } from '@/entities/Todo';
+import path from 'path';
+import fs from 'fs';
+
+const dbPath = process.env.DATABASE_PATH || './data/todos.db';
+const resolvedDbPath = path.resolve(process.cwd(), dbPath);
+const dbDir = path.dirname(resolvedDbPath);
+
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __dataSource: DataSource | undefined;
+}
+
+const AppDataSource = global.__dataSource ??
+  new DataSource({
+    type: 'better-sqlite3',
+    database: resolvedDbPath,
+    synchronize: true,
+    logging: false,
+    entities: [Todo],
+  });
+
+if (!global.__dataSource) {
+  global.__dataSource = AppDataSource;
+}
+
+export async function getDataSource(): Promise<DataSource> {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize();
+  }
+  return AppDataSource;
+}
+
+export default AppDataSource;
